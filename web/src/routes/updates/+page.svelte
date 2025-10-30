1e61cd7b80bfaf552471e57809a5a4e40f1e8b92
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-30 17:07:48 +0530

    Update +page.svelte

--- web/src/routes/updates/+page.svelte
+++ web/src/routes/updates/+page.svelte
@@ -80,11 +80,11 @@
 
 <svelte:head>
     <title>
-        {$t("tabs.updates")} ~ {$t("general.cobalt")}
+        {$t("tabs.updates")} ~ {$t("general.kac")}
     </title>
     <meta
         property="og:title"
-        content="{$t("tabs.updates")} ~ {$t("general.cobalt")}"
+        content="{$t("tabs.updates")} ~ {$t("general.kac")}"
     />
 </svelte:head>
 
