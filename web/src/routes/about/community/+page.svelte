1f2b69db1de0921a11a2e9b9635c2e031a3cc732
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-30 14:28:23 +0530

    Update +page.svelte

--- web/src/routes/about/community/+page.svelte
+++ web/src/routes/about/community/+page.svelte
@@ -17,28 +17,9 @@
         class="two"
         class:one={buttonContainerWidth < 500}
     >
-        <AboutSupport
-            platform="github"
-            externalLink={contacts.github}
-        />
-
-        {#if $locale === "ru"}
             <AboutSupport
-                platform="telegram"
-                externalLink={contacts.telegram_ru}
-            />
-        {:else}
-            <AboutSupport
-                platform="discord"
-                externalLink={contacts.discord}
-            />
-            <AboutSupport
-                platform="twitter"
-                externalLink={contacts.twitter}
-            />
-            <AboutSupport
-                platform="bluesky"
-                externalLink={contacts.bluesky}
+                platform="email"
+                window.open("mailto:hcaresyou@gmail.com");}
             />
         {/if}
     </div>
