17bee69afcbf20115c1d71454fcd33cca222e03b
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-31 11:51:11 +0530

    Update config.js

--- api/src/config.js
+++ api/src/config.js
@@ -7,7 +7,7 @@ const canonicalEnv = Object.freeze(structuredClone(process.env));
 const env = loadEnvs();
 
 const genericUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
-const cobaltUserAgent = `cobalt/${version} (+https://github.com/imputnet/cobalt)`;
+const cobaltUserAgent = `cobalt/${version} (+https://github.com/1Ltd/kac)`;
 
 export const setTunnelPort = (port) => env.tunnelPort = port;
 export const isCluster = env.instanceCount > 1;
