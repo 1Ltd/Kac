1f1caacf97cb1e233f8fde066e544444ef34b327
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-31 10:48:36 +0530

    Update api.js

--- api/src/core/api.js
+++ api/src/core/api.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import http from "node:http";
 import rateLimit from "express-rate-limit";
 import { setGlobalDispatcher, EnvHttpProxyAgent } from "undici";
-import { getCommit, getBranch, getRemote, getVersion } from "@imput/version-info";
+import { getRemote, getVersion } from "@imput/version-info";
 
 import jwt from "../security/jwt.js";
 import stream from "../stream/stream.js";
@@ -25,8 +25,6 @@ import * as Cookies from "../processing/cookie/manager.js";
 import * as YouTubeSession from "../processing/helpers/youtube-session.js";
 
 const git = {
-    branch: await getBranch(),
-    commit: await getCommit(),
     remote: await getRemote(),
 }
 
